1156e617f183ad4fc86aa3ff2a563474ca214145
EvgeniySh77 <evgen.sh77@mail.ru>
2022-02-21 01:06:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace task_8_2_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\INTENSO\Desktop\Techno");
+            string newPath = @"C:\$Recycle.Bin\Techno";
+
+            dirInfo.MoveTo(newPath);
+        }
+    }
+}
